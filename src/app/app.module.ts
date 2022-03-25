8f28a6e077859e6163038340752ce1897d4f6f35
YashwanthC <cyashwanthkumar@presidio.com>
2022-03-25 15:09:02 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackgroundComponent } from './components/background/background.component';
+import { DescComponent } from './components/desc/desc.component';
+import { MeetMeComponent } from './components/meet-me/meet-me.component';
+import { LocationComponent } from './components/location/location.component';
+import { NgxArcTextModule } from 'ngx-arc-text';
+import { LogoComponent } from './components/logo/logo.component';
+import { MeetMeLineComponent } from './components/meet-me-line/meet-me-line.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent,
+    DescComponent,
+    MeetMeComponent,
+    LocationComponent,
+    LogoComponent,
+    MeetMeLineComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgxArcTextModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
